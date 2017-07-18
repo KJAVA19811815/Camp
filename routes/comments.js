7e62ea613563b2f753de36cb601b5eeb5a568259
KJAVA19811815 <karunsingh1993@gmail.com>
2017-07-18 23:06:33 +0000

    middle for comment and authorization for comment done

--- routes/comments.js
+++ routes/comments.js
@@ -40,7 +40,7 @@ router.post('/campgrounds/:id/comments', isLoggedIn, (req,res) => {
   })
 })
 
-router.get('/campgrounds/:id/comments/:comment_id/edit', (req,res) => {
+router.get('/campgrounds/:id/comments/:comment_id/edit', checkCommentOwnership, (req,res) => {
   Comment.findById(req.params.comment_id, function(err, foundComment){
     if(err){
       res.redirect("back")
@@ -50,7 +50,7 @@ router.get('/campgrounds/:id/comments/:comment_id/edit', (req,res) => {
   })
 })
 
-router.put('/campgrounds/:id/comments/:comment_id', (req, res) => {
+router.put('/campgrounds/:id/comments/:comment_id', checkCommentOwnership, (req, res) => {
   Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment) {
     if(err){
       res.redirect('/campgrounds');
@@ -60,11 +60,43 @@ router.put('/campgrounds/:id/comments/:comment_id', (req, res) => {
   })
 })
 
+router.delete('/campgrounds/:id/comments/:comment_id', checkCommentOwnership, (req,res) => {
+  Comment.findByIdAndRemove(req.params.comment_id, function(err){
+    if(err){
+      res.redirect('back')
+    } else{
+      res.redirect('/campgrounds/' + req.params.id)
+    }
+  })
+})
+
 function isLoggedIn(req, res, next){
   if(req.isAuthenticated()){
     return next();
   }
   res.redirect('/login')
+}
+
+function checkCommentOwnership(req,res,next){
+  if(req.isAuthenticated()){
+    Comment.findById(req.params.comment_id, function(err, foundComment){
+      if(err){
+        console.log("error")
+      } else {
+        console.log("IIIDIDIDID " + foundComment);
+        console.log("22222222 " + req.user._id)
+        if(foundComment.author.id.equals(req.user._id)){
+          next();
+
+        } else {
+          res.redirect("back")
+        }
+      }
+    })
+  } else {
+    res.redirect("back");
+  }
+
 }
 
 module.exports = router;
